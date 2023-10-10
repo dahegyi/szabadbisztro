e6c73f458771da0c4b0fb0ecef58e8dc3e8df627
dahegyi <dahegyi@proton.me>
2023-10-11 00:20:16 +0200

    last commit before 2.0

--- src/components/content.js
+++ src/components/content.js
@@ -3,9 +3,10 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
 
 const Content = (props) => {
-  const { cms } = props;
+  const { cms, restaurant, setRestaurant } = props;
 
   return (
     <>
@@ -14,6 +15,7 @@ const Content = (props) => {
         <PrismicRichText field={cms?.about_title} className="mb-3" />
         <PrismicRichText field={cms?.about_content} />
       </Container>
+
       {/* Slideshow */}
       {cms?.banners.length > 0 ? (
         <Container>
@@ -30,40 +32,81 @@ const Content = (props) => {
           </Carousel>
         </Container>
       ) : null}
-      {/*  Menu images */}
+
+      {/*  Menu */}
       <Container id="menu" className="text-center mb-5">
-        <div className="text-uppercase mt-5">
-          <PrismicRichText field={cms?.menu_title} />
-        </div>
-        <Row className="mb-5">
-          <Col sm="12" md="6">
-            <img
-              src={cms?.menu_image_left.url}
-              alt=""
-              width="100%"
-              loading="lazy"
-            />
-          </Col>
-          <Col sm="12" md="6">
-            <img
-              src={cms?.menu_image_right.url}
-              alt=""
-              width="100%"
-              loading="lazy"
-            />
-          </Col>
-        </Row>
-        {cms?.menu_image_bottom?.url ? (
-          <img
-            src={cms?.menu_image_bottom.url}
-            alt=""
-            width="100%"
-            className="mb-5"
-            loading="lazy"
+        <div className="text-uppercase mt-5 mb-2">
+          <PrismicRichText
+            field={restaurant === 0 ? cms?.choose_restaurant : cms?.menu_title}
           />
-        ) : null}
-        <PrismicRichText field={cms?.plant_based_disclaimer} />
+        </div>
+        {/*  Restaurant selector */}
+        {restaurant === 0 ? (
+          <Col>
+            <Button variant="link" onClick={() => setRestaurant(1)}>
+              Szabad Bisztró
+            </Button>
+            -
+            <Button variant="link" onClick={() => setRestaurant(2)}>
+              újHely
+            </Button>
+          </Col>
+        ) : (
+          <Container>
+            <Row className="mb-3">
+              <Button variant="link" onClick={() => setRestaurant(0)}>
+                {cms?.back_to_choose_restaurant}
+              </Button>
+            </Row>
+            <Row className="mb-5">
+              <Col sm="12" md="6">
+                <img
+                  src={
+                    restaurant === 1
+                      ? cms?.menu_image_left.url
+                      : cms?.ujhely_menu_image_left.url
+                  }
+                  alt=""
+                  width="100%"
+                  loading="lazy"
+                />
+              </Col>
+              <Col sm="12" md="6">
+                <img
+                  src={
+                    restaurant === 1
+                      ? cms?.menu_image_right.url
+                      : cms?.ujhely_menu_image_right.url
+                  }
+                  alt=""
+                  width="100%"
+                  loading="lazy"
+                />
+              </Col>
+            </Row>
+            {restaurant === 1 && cms?.menu_image_bottom?.url ? (
+              <img
+                src={cms?.menu_image_bottom.url}
+                alt=""
+                width="100%"
+                className="mb-5"
+                loading="lazy"
+              />
+            ) : null}
+            {restaurant === 2 && cms?.ujhely_menu_image_bottom?.url ? (
+              <img
+                src={cms?.ujhely_menu_image_bottom.url}
+                alt=""
+                width="100%"
+                className="mb-5"
+                loading="lazy"
+              />
+            ) : null}
+            <PrismicRichText field={cms?.plant_based_disclaimer} />
+          </Container>
+        )}
       </Container>
+
       {/*  Video slideshow */}
       {cms?.videos.length > 0 ? (
         <Container className="mt-2 mb-5">
@@ -74,8 +117,8 @@ const Content = (props) => {
                   className="iframe"
                   src={`https://www.youtube-nocookie.com/embed/${video.youtube_video_id}`}
                   title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
                   allowFullScreen
                 />
               </Carousel.Item>
@@ -83,6 +126,7 @@ const Content = (props) => {
           </Carousel>
         </Container>
       ) : null}
+
       {/*  Collage */}
       {cms?.menu_image_bottom_2?.url ? (
         <img
@@ -92,16 +136,31 @@ const Content = (props) => {
           className="mb-5"
         />
       ) : null}
+
       {/*  Table reservation */}
-      <div id="contact" className="contact-container">
-        <PrismicRichText field={cms?.table_reservation_title} />
-        <a href={`tel:${cms?.phone_number}`}>{cms?.phone_number}</a>
-      </div>
-      {/*  Opening hours */}
-      {cms?.opening_hours_title && cms?.opening_hours_content ? (
-        <div className="opening-hours">
-          <PrismicRichText field={cms?.opening_hours_title} />
-          <PrismicRichText field={cms?.opening_hours_content} />
+      {restaurant > 0 ? (
+        <div className="contact-container">
+          <div id="contact" className="reservation">
+            <PrismicRichText field={cms?.table_reservation_title} />
+            <a
+              href={`tel:${
+                restaurant === 1 ? cms?.phone_number : cms?.ujhely_phone_number
+              }`}
+            >
+              {restaurant === 1 ? cms?.phone_number : cms?.ujhely_phone_number}
+            </a>
+          </div>
+          {/*  Opening hours */}
+          <div className="opening-hours">
+            <PrismicRichText field={cms?.opening_hours_title} />
+            <PrismicRichText
+              field={
+                restaurant === 1
+                  ? cms?.opening_hours_content
+                  : cms?.ujhely_opening_hours_content
+              }
+            />
+          </div>
         </div>
       ) : null}
     </>
